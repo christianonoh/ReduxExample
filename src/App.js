14d7f669c5020c3d2780627a4a95ba91ceccd5f8
christianonoh <onohchibyk@gmail.com>
2023-05-10 13:16:47 +0100

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { calculateTotal } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
   return (
     <div className="App">
-      <h1>Hi Burger</h1>
+      <Navbar />
+      <Modal />
+      <CartContainer />
     </div>
   );
 }
