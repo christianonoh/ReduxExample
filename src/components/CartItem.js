a80078732bbd86088b49d0daee455a4878f5dd40
christianonoh <onohchibyk@gmail.com>
2023-05-10 13:02:54 +0100

    Add cart item component

---
+++ src/components/CartItem.js
@@ -0,0 +1,46 @@
+import { BsChevronUp, BsChevronDown } from 'react-icons/bs';
+import { useDispatch } from 'react-redux';
+import { increase, removeItem, decrease } from '../features/cart/cartSlice';
+
+const CartItem = ({
+  id, title, price, img, amount,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">
+          $
+          {price}
+        </h4>
+        <button
+          type="button"
+          className="remove-btn"
+          onClick={() => { dispatch(removeItem(id)); }}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button type="button" className="amount-btn" onClick={() => dispatch(increase({ id }))}>
+          <BsChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button
+          type="button"
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) return dispatch(removeItem(id));
+            dispatch(decrease({ id }));
+          }}
+        >
+          <BsChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
